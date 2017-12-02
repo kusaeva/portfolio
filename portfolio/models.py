ccf25cc7651066770fd4ebd21431851ef5cb732c
Olga Borzenkova <kyssike@gmail.com>
2017-12-02 22:35:32 +0300

    test media files

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class HeaderPhoto(models.Model):
+    photo = models.ImageField(upload_to='img/header/')
+    description = models.CharField(max_length=32)
+    is_active = models.BooleanField(default=True)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from .models import HeaderPhoto
 
 
 def index(request):
-    context = {}
+    header_photo = HeaderPhoto.objects.filter(is_active=True)[0]
+    context = {'header_photo': header_photo}
     return render(request, 'portfolio/index.html', context)
