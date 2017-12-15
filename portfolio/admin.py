7b3efaca3d30f54ee50df3c62acdf1bab05059a1
Olga Borzenkova <kyssike@gmail.com>
2017-12-15 21:09:43 +0300

    add other models

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -7,12 +7,13 @@ from django.shortcuts import render
 from django.contrib.admin import helpers
 from django.http import HttpResponseRedirect
 
-from photologue.admin import PhotoEffect, Watermark, Photo
+from photologue.admin import PhotoEffect, Watermark
 from photologue.admin import PhotoAdmin as PhotoAdminDefault
 from photologue.admin import GalleryAdmin as GalleryAdminDefault
 
 from .forms import UploadZipForm
-from photologue.models import Gallery
+from photologue.models import Gallery, Photo
+from .models import Lovestory, Family, Portrait, Travel
 
 
 class PhotoAdminForm(forms.ModelForm):
@@ -29,6 +30,46 @@ class GalleryAdminForm(forms.ModelForm):
 
 class GalleryAdmin(GalleryAdminDefault):
     form = GalleryAdminForm
+
+
+class LovestoryAdminForm(forms.ModelForm):
+    class Meta:
+        model = Lovestory
+        exclude = ('sites', 'description', 'date_added',)
+
+
+class LovestoryAdmin(GalleryAdminDefault):
+    form = LovestoryAdminForm
+
+
+class FamilyAdminForm(forms.ModelForm):
+    class Meta:
+        model = Family
+        exclude = ('sites', 'description', 'date_added',)
+
+
+class FamilyAdmin(GalleryAdminDefault):
+    form = FamilyAdminForm
+
+
+class TravelAdminForm(forms.ModelForm):
+    class Meta:
+        model = Travel
+        exclude = ('sites', 'description', 'date_added',)
+
+
+class TravelAdmin(GalleryAdminDefault):
+    form = TravelAdminForm
+
+
+class PortraitAdminForm(forms.ModelForm):
+    class Meta:
+        model = Portrait
+        exclude = ('sites', 'description', 'date_added',)
+
+
+class PortraitAdmin(GalleryAdminDefault):
+    form = PortraitAdminForm
 
 
 class PhotoAdmin(PhotoAdminDefault):
@@ -68,3 +109,7 @@ admin.site.unregister(Watermark)
 
 admin.site.register(Photo, PhotoAdmin)
 admin.site.register(Gallery, GalleryAdmin)
+admin.site.register(Lovestory, GalleryAdmin)
+admin.site.register(Family, GalleryAdmin)
+admin.site.register(Travel, GalleryAdmin)
+admin.site.register(Portrait, GalleryAdmin)
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.db import models
-from django.conf import settings
-
-class HeaderPhoto(models.Model):
-    photo = models.ImageField(upload_to='img/header/')
-    description = models.CharField(max_length=32)
-    is_active = models.BooleanField(default=True)
+from photologue.models import Gallery as GalleryDefault
 
 
+class Lovestory(GalleryDefault):
+    pass
 
-class Story(models.Model):
-    cover = models.CharField(max_length=16)
-    name = models.CharField(max_length=16)
 
-class Photo(models.Model):
-    name = models.CharField(max_length=16)
-    story = models.ForeignKey(Story, on_delete=models.CASCADE)
+class Travel(GalleryDefault):
+    pass
+
+
+class Family(GalleryDefault):
+    pass
+
+
+class Portrait(GalleryDefault):
+    pass
